49ccb5e0f5c8e6a749c5759d1205141b24754366
Ari Meyer <ari.f.meyer@gmail.com>
2023-11-12 17:02:03 -0600

    resubmission commit

--- sql_in_action-finished.sql
+++ sql_in_action-finished.sql
@@ -4,6 +4,7 @@
 
 -- SELECT * FROM final_airbnb;
 
+USE intro_sql;
 
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 1 >>>>>>>>>>>>>>>>>>>>>>>
@@ -24,6 +25,8 @@ WHERE host_id = 63613;
 -- Query the data to just show the unique neighbourhoods listed
 -- HINT: This is a "distinct" operation...
 
+SELECT DISTINCT neighbourhood FROM final_airbnb;
+
 -- EXPECTED OUTPUT: 40 neighbourhoods listed
 
 
@@ -35,14 +38,19 @@ WHERE host_id = 63613;
 -- FOOD FOR THOUGHT: Think about the results. Are the high and low prices outliers in this data set?
 
 -- EXPECTED OUTPUT: Highest = 785, Lowest = 55
+SELECT MAX(price)
+FROM final_airbnb;
 
+SELECT MIN(price)
+FROM final_airbnb;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 5 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find the average availability for all listings in the data set (using the availability_365 column)
 -- HINT: Aggregates are more than just big rocks...
 
 -- EXPECTED OUTPUT: 165.3904
-
+SELECT AVG(availability_365)
+FROM final_airbnb;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 6 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find all listings that do NOT have a review
@@ -50,12 +58,19 @@ WHERE host_id = 63613;
 
 -- EXPECTED OUTPUT: 6 rows
 
+SELECT number_of_reviews
+FROM final_airbnb
+WHERE number_of_reviews = 0; -- doesn't accept WHERE number_of_reviews IS NULL; ???
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 7 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find the id of the listing with a room_type of "Private room" that has the most reviews 
 -- HINT: Sorting is your friend!
 
 -- EXPECTED OUTPUT: 58059
+SELECT id FROM final_airbnb
+WHERE number_of_reviews = 605 AND room_type = 'Private room';
+
+
 
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 8 >>>>>>>>>>>>>>>>>>>>>>>
@@ -66,11 +81,22 @@ WHERE host_id = 63613;
 -- EXPECTED OUTPUT: Williamsburg
 -- INVESTIGATE: Should Williamsburg be crowned the most popular neighbourhood?
 
+SELECT neighbourhood, COUNT(*) AS num
+FROM `intro_sql`.`final_airbnb`
+GROUP BY neighbourhood
+ORDER BY num DESC
+LIMIT 1;
+
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 9 >>>>>>>>>>>>>>>>>>>>>>>
 -- Query the data to discover which listing is the most popular using the reviews_per_month for all listings with a minimum_nights value of less than 7
 -- HINT: Sorting is still your friend! So are constraints.
 
 -- EXPECTED OUTPUT: 58059
+SELECT id FROM intro_sql.final_airbnb
+WHERE minimum_nights < 7
+order by reviews_per_month desc
+limit 1;
+
 
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 10 >>>>>>>>>>>>>>>>>>>>>>>
@@ -81,14 +107,35 @@ WHERE host_id = 63613;
 
 -- EXPECTED OUTPUT: The Box House Hotel with 6 listings
 
+SELECT host_id, host_name, COUNT(*) AS listings
+FROM `intro_sql`.`final_airbnb`
+GROUP BY host_id, host_name
+ORDER BY listings DESC
+LIMIT 1;
+
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 11 >>>>>>>>>>>>>>>>>>>>>>>
 -- <<<<<<<<<<<<<<<<<<<<<<< WRAP UP >>>>>>>>>>>>>>>>>>>>>>>>>
 -- What do you think makes a successful AirBnB rental in this market? What factors seem to be at play the most?
 -- Write a few sentences and include them with your project submission in the README file 
 
+-- Assuming each renter logs a review, we can calculate a minimum revenue per listing per month by multiplying minimum nights, reviews per month and price. 
+-- We don't know how much each listing costs to operate but we do know the revenue, so assuming the margin is roughly the same, the revenue corolates with how successful that Airbnb rental is.
+
+SELECT price, minimum_nights, reviews_per_month,
+price * minimum_nights * reviews_per_month AS minimum_revenue
+FROM intro_sql.final_airbnb
+order by minimum_revenue DESC;
+
 
 -- <<<<<<<<<<<<<<<<<<<<< ** BONUS ** >>>>>>>>>>>>>>>>>>>>>>>
--- Find the the percent above or below each listing is compared to the average price for all listings.
+-- Find the percent above or below each listing is compared to the average price for all listings.
 -- HINT: No hints! It's a bonus for a reason :)
+
+select
+price,
+(SELECT AVG(price) FROM intro_sql.final_airbnb) as average_price,
+(price / (SELECT AVG(price) FROM intro_sql.final_airbnb) - 1) * 100 as percent_diff
+FROM
+intro_sql.final_airbnb;
 
